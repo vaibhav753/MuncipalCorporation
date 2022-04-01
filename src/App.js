003b44430c1b746363d335b5474fc685c2cd0920
Darshan Nangare <nangaredarshan@gmail.com>
2022-04-01 18:37:20 +0530

    new.txt updated

--- src/App.js
+++ src/App.js
@@ -22,44 +22,48 @@ import ComplaintList from './Components/complaints/ComplaintList';
 import ComplaintsCompletedList from './Components/complaints/ComplaintsCompletedList';
 import ComplaintsRejectedList from './Components/complaints/ComplaintsRejectedList';
 import HODOnlineServices from './Components/hod/HODOnlineServices';
-import CovidApp from './Components/CovidTracker/CovidApp';
-
-
+import Table from './Components/payment/Table';
+import UVList from './Components/onlineServices/UVList';
+import VList from './Components/onlineServices/VList';
+import RejectedList from './Components/onlineServices/RejectedList';
 function App() {
 
   return (
     <BrowserRouter>
+      <div >
+      <NavigationBar /><br/><br/>
+      
+        <Routes>
+         
+          <Route path='/' element={<HomePage />} />
+          <Route path='/complaints' element={<Complaint />} />
+          
+          <Route path='/onlineServices' element={<OnlineServices/>}></Route>
+          <Route path='/birthRegistration' element={<BirthRegistration/>}></Route>
+          <Route path='/marriageRegistration' element={<MarriageRegistration/>}></Route>
+          <Route path='/deathRegistration' element={<DeathRegistration/>}></Route>
+          <Route path='/aboutUs' element={<AboutUs/>}></Route>
+          <Route path='/contact' element={<Contact/>}></Route>
+          <Route path='/deathRegistration' element={<Registration/>}></Route>
+          <Route path='/login' element={<Login/>}></Route>
+          <Route path='/newpayment' element={<NewPayment/>}></Route>
+          <Route path='/registration' element={<Registration/>}></Route>
+          <Route path='/touristGarden'element={<TouristGarden/>}></Route>
+          <Route path='/touristZoo' element={<TouristZoo/>}></Route>
+          <Route path='/hodhomepage' element={<HODHomePage/>}></Route>
+          <Route path='/complaintslist' element={<ComplaintList/>}></Route>
+          <Route path='/complaintscompletedlist' element={<ComplaintsCompletedList />}></Route>
+          <Route path='/complaintsrejectedlist' element={<ComplaintsRejectedList />}></Route>
+          <Route path='/HODOnlineServices' element={<HODOnlineServices/>}></Route>
+          <Route path='/table' element={<Table/>}></Route>
+          <Route path='/uvlist' element={<UVList/>}></Route>
+          <Route path='/vlist' element={<VList/>}></Route>
+          <Route path='/rejectedlist' element={<RejectedList/>}></Route>
+          
 
-      <NavigationBar />
-
-      <Routes>
-        covidTracker
-        <Route path='/' element={<HomePage />} />
-        <Route path='/covidTracker' element={<CovidApp />} />
-        <Route path='/complaints' element={<Complaint />} />
-
-        <Route path='/onlineServices' element={<OnlineServices />}></Route>
-        <Route path='/birthRegistration' element={<BirthRegistration />}></Route>
-        <Route path='/marriageRegistration' element={<MarriageRegistration />}></Route>
-        <Route path='/deathRegistration' element={<DeathRegistration />}></Route>
-        <Route path='/aboutUs' element={<AboutUs />}></Route>
-        <Route path='/contact' element={<Contact />}></Route>
-        <Route path='/deathRegistration' element={<Registration />}></Route>
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/newpayment' element={<NewPayment />}></Route>
-        <Route path='/registration' element={<Registration />}></Route>
-        <Route path='/touristGarden' element={<TouristGarden />}></Route>
-        <Route path='/touristZoo' element={<TouristZoo />}></Route>
-        <Route path='/hodhomepage' element={<HODHomePage />}></Route>
-        <Route path='/complaintslist' element={<ComplaintList />}></Route>
-        <Route path='/complaintscompletedlist' element={<ComplaintsCompletedList />}></Route>
-        <Route path='/complaintsrejectedlist' element={<ComplaintsRejectedList />}></Route>
-        <Route path='/HODOnlineServices' element={<HODOnlineServices />}></Route>
-
-
-      </Routes>
-
-
+        </Routes>
+        
+      </div>
       <Footer></Footer>
     </BrowserRouter>
 
